8b20593d23421009e7832c908e44f2478357013f
salmanabdellatif <salman.mo20201@gmail.com>
2026-01-15 23:25:58 +0300

    feat(cli): add logout command

---
+++ apps/cli/src/commands/logout.ts
@@ -0,0 +1,20 @@
+import { Command } from "commander";
+import chalk from "chalk";
+import { configManager } from "../lib/config.js";
+import { logger } from "../utils/logger.js";
+
+export const logoutCommand = new Command("logout")
+  .description("Log out and clear local credentials")
+  .action(async () => {
+    if (!configManager.isAuthenticated()) {
+      logger.info(chalk.yellow("You are already logged out."));
+      return;
+    }
+
+    configManager.clear();
+
+    const email = configManager.getUserEmail();
+    configManager.clear();
+    console.log(chalk.green(`\nLogged out from ${email}.`));
+    console.log(chalk.gray("Local credentials have been removed."));
+  });
--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 import { Command } from "commander";
+import { loginCommand } from "./commands/login.js";
+import { logoutCommand } from "./commands/logout.js";
 
 const program = new Command();
 
 program
   .name("envsync")
-  .description("CLI for EnvSync - Manage your environment secrets")
-  .version("1.0.0");
+  .description("CLI for EnvSync - E2E encrypted secrets management")
+  .version("0.1.0");
 
-program
-  .command("login")
-  .description("Login to EnvSync")
-  .action(() => {
-    console.log("Login command executed!");
-  });
+// Register commands
+program.addCommand(loginCommand);
+program.addCommand(logoutCommand);
 
 program.parse(process.argv);
