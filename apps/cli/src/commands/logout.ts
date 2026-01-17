58d9d81aa2898e89e46c7fdb71648bd1ec927404
salmanabdellatif <salman.mo20201@gmail.com>
2026-01-17 22:17:47 +0300

    refactor(cli): update logout for new config

--- apps/cli/src/commands/logout.ts
+++ apps/cli/src/commands/logout.ts
@@ -1,17 +1,16 @@
 import { Command } from "commander";
 import chalk from "chalk";
 import { configManager } from "../lib/config.js";
-import { logger } from "../utils/logger.js";
 
 export const logoutCommand = new Command("logout")
   .description("Log out and clear local credentials")
   .action(async () => {
     if (!configManager.isAuthenticated()) {
-      logger.info(chalk.yellow("You are already logged out."));
+      console.log(chalk.yellow("You are already logged out."));
       return;
     }
 
-    const email = configManager.getUserEmail();
+    const email = configManager.getUser()?.email;
     configManager.clear();
     console.log(chalk.green(`\nLogged out from ${email}.`));
     console.log(chalk.gray("Local credentials have been removed."));
